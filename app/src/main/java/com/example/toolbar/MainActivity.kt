22635ad3ae58c44397e9ed2dbca0d667bf4e6e3d
05443 <0544354125m@gmail.com>
2021-06-17 13:32:31 +0300

    this testing

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.toolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val thisId =findViewById<TextView>(R.id.this_Id)
+        thisId.setOnClickListener {
+            Toast.makeText(this,"kjjjjjjjjjjjjjjjjj",Toast.LENGTH_LONG).show()
+        }
     }
 }
